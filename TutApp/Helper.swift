6a400e155454bbc99e02b30c4df1dc08e6b99963
jishnu b <jishnu@inapp.com>
2016-02-13 17:59:00 +0530

    stable version with out pen

---
+++ TutApp/Helper.swift
@@ -0,0 +1,118 @@
+
+//
+//  Helper.swift
+//  TutApp
+//
+//  Created by jishnu b on 11/02/16.
+//  Copyright © 2016 jishnu b. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//CGContextRef context = UIGraphicsGetCurrentContext();
+//UIColor *currentColor = [UIColor blackColor];
+//CGContextSetStrokeColorWithColor(context, currentColor.CGColor);
+//CGContextSetLineWidth(context, 2.0);
+//CGContextBeginPath(context);
+//CGContextMoveToPoint(context, touchStart.x, touchStart.y);
+//CGContextAddLineToPoint(context, touchEnd.x, touchEnd.y);
+//CGContextStrokePath(context);
+//
+
+
+func DrawLine( From from:CGPoint, To to:CGPoint, WithColor color:UIColor ,OnView view:UIView){
+    UIGraphicsBeginImageContextWithOptions(view.frame.size, false, 0)
+   // UIGraphicsBeginImageContext(view.frame.size);
+    let  context = UIGraphicsGetCurrentContext()
+    var currentColor =  UIColor.blackColor()
+    CGContextSetStrokeColorWithColor(context, currentColor.CGColor)
+    CGContextSetLineWidth(context, 2.0)
+    CGContextBeginPath(context)
+    CGContextMoveToPoint(context, from.x, from.y);
+    CGContextAddLineToPoint(context, to.x, to.y);
+    CGContextStrokePath(context);
+    
+
+}
+
+
+func CreatePathLayerAndPenLayerAndAddAnimation(OnLayer layer:CALayer  ,ForVC viewController:UIViewController, ForPath  path:UIBezierPath){
+    //create layers
+ //   let pathLayer = CreateAndReturnPathLayer(OnLayer: layer :path)
+    
+    let pathLayer = CreateAndReturnPathLayer(OnLayer: layer, path: path)
+    let penLayer = CreatePenLayer(OnLayer:pathLayer )
+    
+    //add animations
+    AddAnimationToPathLayer(pathLayer)
+    AddAnimationToPenLayer(WithPathLayer:pathLayer , ForVC: viewController)
+}
+
+
+
+
+func CreateAndReturnPathLayer(OnLayer layer:CALayer  , path:UIBezierPath)->CAShapeLayer{
+    
+    let pathLayer = CAShapeLayer()
+    pathLayer.frame =  layer.bounds
+   // pathLayer.bounds =  CGPathGetBoundingBox(path.CGPath)
+    pathLayer.bounds = layer.bounds
+    pathLayer.geometryFlipped = false;
+    pathLayer.path = path.CGPath;
+    pathLayer.strokeColor =  UIColor.blackColor().CGColor
+    pathLayer.fillColor = nil
+    pathLayer.lineWidth = 3.0
+    pathLayer.lineJoin = kCALineJoinBevel
+    layer.addSublayer(pathLayer)
+    return pathLayer
+    
+}
+
+
+func CreatePenLayer(OnLayer layer:CAShapeLayer){
+    
+    let  penImage = UIImage(named: "pen")
+    let penLayer = CALayer()
+    penLayer.contents = penImage?.CGImage
+    penLayer.anchorPoint = CGPointZero
+    penLayer.frame =  CGRectMake(0, 0, (penImage?.size.width)!, (penImage?.size.height)!)
+    layer.addSublayer(penLayer)
+}
+
+
+func AddAnimationToPathLayer(layer:CALayer){
+    
+    let pathAnimation = CABasicAnimation(keyPath: "strokeEnd")
+    pathAnimation.duration = 10.0
+    pathAnimation.fromValue =    NSNumber(float: 0.0)
+    pathAnimation.toValue =    NSNumber(float: 1.0)
+    layer.addAnimation(pathAnimation, forKey: "strokeEnd")
+    
+}
+
+
+func AddAnimationToPenLayer(WithPathLayer layer:CAShapeLayer ,  ForVC viewController:UIViewController){
+    
+    
+    let penAnimation = CAKeyframeAnimation(keyPath: "position")
+    penAnimation.duration = 10.0
+    penAnimation.path = layer.path;
+    penAnimation.calculationMode = kCAAnimationPaced;
+    penAnimation.delegate = viewController
+    layer.addAnimation(penAnimation, forKey: "position")
+    
+}
+
+//func AppendCGMutablePathRefToUIBezierPath(ForString   pathString:String)->CGPath{
+//
+//
+//    let  pathCreated = attributedTextObjC.getAttributedTextPath(pathString)
+//    let path = UIBezierPath()
+//    path.moveToPoint(CGPointZero)
+//    path.appendPath(UIBezierPath(CGPath: pathCreated) )
+//
+//}
+
+
+
--- TutApp/ViewController.swift
+++ TutApp/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    var startPoint = CGPoint()
+    var animationLayer = CALayer()
+    var path = UIBezierPath()
+    var penLayer : CALayer?
+    let pathLayer = CAShapeLayer()
+    
+    
+    @IBOutlet var btn: UIButton!
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    override func viewDidAppear(animated: Bool) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            startPoint = touch.locationInView(self.view)
+            path.moveToPoint(startPoint)
+            
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            
+            let currentPoint = touch.locationInView(self.view)
+            path.addLineToPoint(currentPoint)
+            CreateAndReturnPathLayer(OnLayer: self.view.layer, path: path)
+           
+            
+        }
+        
+    }
+    
+    func CreateAndReturnPathLayerOnVC( path:UIBezierPath){
+        
+        pathLayer.frame =  self.view.layer.bounds
+        pathLayer.bounds = self.view.layer.bounds
+        pathLayer.geometryFlipped = false;
+        pathLayer.path = path.CGPath;
+        pathLayer.strokeColor =  UIColor.blackColor().CGColor
+        pathLayer.fillColor = nil
+        pathLayer.lineWidth = 3.0
+        pathLayer.lineJoin = kCALineJoinMiter
+        self.view.layer.addSublayer(pathLayer)
+        
+    }
+    
 }
 
